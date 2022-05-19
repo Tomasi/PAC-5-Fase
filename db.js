0fd72f16cd26864dc93bf5dab4da9a5cd191b4a0
Igor Tomasi <igor.tomasi@catolicasc.org.br>
2022-05-19 00:11:34 -0300

    Incluída Gravação, leitura e update dos integrantes.

--- db.js
+++ db.js
@@ -10,10 +10,10 @@ async function conectarBD()
     const connection = await mysql.createConnection(
         {
             host     : 'localhost',
-            port     : 5432,
+            port     : 3306,
             user     : 'root',
             password : '',
-            database : 'applications'
+            database : 'PAC'
         }
     );
 
@@ -21,4 +21,30 @@ async function conectarBD()
     return global.connection
 }
 
-//module.exports = {}
+async function consultaIntegrantes(){
+    const conexao = await conectarBD()
+    const [registros] = await conexao.query("select * from integrante;")
+    return registros
+}
+
+async function consultaIntegrante(codigo){
+    const conexao = await conectarBD()
+    const sql = "select * from integrante where int_codigo = (?);"
+    const [integrantes] = await conexao.query(sql, [codigo])
+    return integrantes[0]
+}
+
+async function saveIntegrante(integrante){
+    const conexao = await conectarBD()
+    const sql = "insert into integrante (int_nome, int_senha, int_email, int_telefone) values (?,?,?,?);"
+    return await conexao.query(sql, [integrante.nome, integrante.senha, integrante.email, integrante.telefone])
+}
+
+async function updateIntegrante(integrante){
+    const conexao = await conectarBD()
+    const sql = "update integrante set int_nome = (?), int_senha = (?), int_email = (?), int_telefone = (?) where int_codigo = (?);"
+    return await conexao.query(sql, [integrante.nome, integrante.senha, integrante.email, integrante.telefone, integrante.codigo]);
+}
+  
+
+module.exports = { saveIntegrante, consultaIntegrantes, consultaIntegrante, updateIntegrante }
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,41 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Express' });
+router.get('/', async function(req, res) {
+  const regIntegrantes = await global.db.consultaIntegrantes()
+  res.render('homePage', { integrantes: regIntegrantes });
+
 });
+
+router.get('/alterarIntegrante/:id', async function(req, res){
+  const codigo = parseInt(req.params.id)
+  const integrante = await global.db.consultaIntegrante(codigo)
+  res.render('manutencaoIntegrantes', {acao: '/updateIntegrante/' + codigo, integrante})
+})
+
+router.get('/cadastrarIntegrante', function(req, res){
+  res.render('manutencaoIntegrantes', {acao: '/salvarIntegrante'});
+});
+
+router.post('/salvarIntegrante', async function(req, res){
+  const nome = req.body.edtNome
+  const email = req.body.edtEmail
+  const telefone = req.body.edtTelefone
+  const senha = req.body.edtSenha
+
+  await global.db.saveIntegrante({nome, email, telefone, senha});
+  res.redirect('/');
+})
+
+router.post('/updateIntegrante/:id', async function(req, res){
+  const codigo = parseInt(req.params.id)
+  const nome = req.body.edtNome
+  const email = req.body.edtEmail
+  const telefone = req.body.edtTelefone
+  const senha = req.body.edtSenha
+
+  await global.db.updateIntegrante({nome, email, telefone, senha, codigo});
+  res.redirect('/');
+})
 
 module.exports = router;
